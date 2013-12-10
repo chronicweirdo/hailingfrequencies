3b6b38bfe2e342c4f059b56f6d9df231f5421089
chronicweirdo <chronicweirdo@gmail.com>
2013-12-10 23:06:54 +0200

    trying to open file from shortcut

--- src/com/chronicweirdo/hailingfrequencies/MyReceiver.java
+++ src/com/chronicweirdo/hailingfrequencies/MyReceiver.java
@@ -1,5 +1,6 @@
 package com.chronicweirdo.hailingfrequencies;
 
+import java.io.File;
 import java.util.Random;
 
 import android.app.PendingIntent;
@@ -8,6 +9,7 @@ import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.util.Log;
 import android.widget.RemoteViews;
 
@@ -43,15 +45,27 @@ public class MyReceiver extends AppWidgetProvider {
 			intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
 			intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
 			
-			PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+			Intent openIntent = new Intent(context, MyReceiver.class);
+			openIntent.setAction(Intent.ACTION_VIEW);
+			openIntent.setType("application/txt");
+			//openIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+			Uri uri = Uri.fromFile(new File("/storage/sdcard0/mydata/cumparaturi.txt"));
+			Log.w("widgetExample", uri.toString());
+			openIntent.setData(uri);
+			
+			//PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, openIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+			PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, openIntent, 0);
 			remoteViews.setOnClickPendingIntent(R.id.update, pendingIntent);
+			//remoteViews.setOnClickFillInIntent(R.id.update, openIntent);
 			appWidgetManager.updateAppWidget(widgetId, remoteViews);
+			
+			
+			
+			//context.startActivity(openIntent);
 		}
 		
 	}
-
-
-
+	
 	/*@Override
 	public void onReceive(Context context, Intent intent) {
 		// TODO: This method is called when the BroadcastReceiver is receiving
